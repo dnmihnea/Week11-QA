9cf0585be563732f059a27063fd787c544ebb417
dnmihnea <mihnea.dinu100@gmail.com>
2022-08-30 17:20:39 +0100

    second exercise

---
+++ react-app/src/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer>
+            <nav className="navbar navbar-expand-sm">
+                <a
+                    className="nav-brand"
+                    href="http://www.qa.com"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <img alt="logo" src="./conk.jpeg" width="50px" height="50px"></img>
+                </a>
+            </nav>
+        </footer>
+    );
+}
+export default Footer;
